8d2d8c48b43ed584b98eb0889a72791c8425c7cd
Tucker Polomik <t.polomik@cablelabs.com>
2023-02-01 09:02:07 -0700

    main: implement rssi-plot callback
    For the currently clicked station, grab RSSI values relative to both the
    Agent it is connected to, as well as Radios that have 'heard' or
    'sniffed' it for unassociated RSSI.
    
    Signed-off-by: Tucker Polomik <t.polomik@cablelabs.com>

--- main.py
+++ main.py
@@ -24,10 +24,12 @@ from topology import Topology
 from nbapi_rpc import *
 from controller_ctx import ControllerConnectionCtx
 from http_auth import HTTPBasicAuthParams
+from colors import ColorSync
 
 app = Dash(__name__)
 app.title = 'CableLabs EasyMesh Network Monitor'
 
+g_StationsToRadio = {}
 class NodeType(Enum):
     UNKNOWN = 0
     STATION = 1
@@ -138,6 +140,22 @@ def add_edge_between_interfaces(iface1: Interface, iface_or_station, edge_interf
     edge_interfaces_y.append(iface_or_station.y)
     edge_interfaces_y.append(None)
 
+def get_color_for_agent(color_selector: ColorSync, agent: Agent) -> str:
+    """Gets a color string for a given agent.
+
+    Args:
+        color_selector (ColorSync): The color selector
+        agent (Agent): The Agent to get a color for
+
+    Returns:
+        str: The string representation of the color to be used for rendering (e.g. 'purple')
+    """
+    agent_mac = agent.get_id()
+    if not color_selector.knows_agent(agent_mac):
+        color_selector.add_agent(agent_mac)
+    return color_selector.get_color_for_agent(agent_mac)
+
+g_ColorSync = ColorSync('green')
 # Agent MAC -> Shape type, for blinking.
 g_RenderState = {}
 def was_last_rendered_as_open_circle(agent: Agent) -> bool:
@@ -235,15 +253,18 @@ def network_graph(topology: Topology):
                     shape_type = 'circle'
                     g_RenderState[agent.get_id()] = shape_type
         node_hover_text.append(gen_node_text(g_Topology, node, G.nodes[node]['type']))
-        if G.nodes[node]['type'] == NodeType.CONTROLLER:
+        node_type = G.nodes[node]['type']
+        if node_type == NodeType.CONTROLLER:
+            agent = topology.get_agent_from_hash(node)
             node_sizes.append(52)
             node_symbols.append(shape_type)
-            node_colors.append('#AA29C5')
+            node_colors.append(get_color_for_agent(g_ColorSync, agent))
             node_labels.append("  prplMesh Controller + Agent<br>  running on prplOs")
         if G.nodes[node]['type'] == NodeType.AGENT:
+            agent = topology.get_agent_from_hash(node)
             node_sizes.append(45)
-            node_symbols.append('circle')
-            node_colors.append('green')
+            node_symbols.append(shape_type)
+            node_colors.append(get_color_for_agent(g_ColorSync, agent))
             if topology.get_agent_from_hash(node).params["ManufacturerModel"] == "Ubuntu": # RDKB
                 node_labels.append("  prplMesh Agent on RDK-B<br>  (Turris-Omnia)")
             elif topology.get_agent_from_hash(node).params["ManufacturerModel"] == "X5042": # ARRIS/ COMMSCOPE 3ʳᵈ
@@ -567,6 +588,14 @@ app.layout = html.Div([
     )
 ])
 
+# RUID -> STA_MAC -> [RSSI measurements]
+g_RSSI_Measurements = {}
+
+stations_have_moved = []
+def handle_station_moved(sta: Station, radio: Radio) -> None:
+    logging.debug(f"Station {sta.get_mac()} has moved! From {g_StationsToRadio[sta.get_mac()]} to {radio.get_ruid()}")
+    stations_have_moved.append(sta.get_mac())
+
 def marshall_nbapi_blob(nbapi_json) -> Topology:
     """Parse a list of NBAPI json entries
 
@@ -699,10 +728,22 @@ def marshall_nbapi_blob(nbapi_json) -> Topology:
                             sta = Station(e['path'], e['parameters'])
                             station_list.append(sta)
                             bss.add_connected_station(sta)
+                            if sta.get_mac() in g_StationsToRadio and g_StationsToRadio[sta.get_mac()] != radio.get_ruid():
+                                handle_station_moved(sta, radio)
+                                del g_StationsToRadio[sta.get_mac()]
+                            g_StationsToRadio[sta.get_mac()] = radio.get_ruid()
                             bss.interface.orientation = ORIENTATION.DOWN
                             # Exclude stations that are actually agents
                             if not bss.interface.get_parent_agent().is_child(sta.get_mac()): #if not bss.interface.is_child(sta.get_mac()):
                                 bss.interface.add_connected_station(sta)
+                            # Append RSSI measurements.
+                            if radio.get_ruid() not in g_RSSI_Measurements:
+                                g_RSSI_Measurements[radio.get_ruid()] = {}
+                            else:
+                                if sta.get_mac() not in g_RSSI_Measurements[radio.get_ruid()]:
+                                    g_RSSI_Measurements[radio.get_ruid()][sta.get_mac()] = []
+                                else:
+                                    g_RSSI_Measurements[radio.get_ruid()][sta.get_mac()].append(sta.get_rssi())
 
     # 8. Check and set stations steering history
     for e in nbapi_json:
@@ -718,12 +759,13 @@ def marshall_nbapi_blob(nbapi_json) -> Topology:
                     if e['path'].startswith(radio.path):
                         unassoc_sta = UnassociatedStation(e['path'], e['parameters'])
                         unassoc_sta.set_parent_radio(radio)
-                        if radio.get_unassociated_station_by_mac(unassoc_sta.get_mac()):
-                            # This is NOT the first time this radio has seen this unassociated station.
-                            # So, update it's params instead of adding a new object.
-                            radio.update_unassociated_station(unassoc_sta, e['parameters'])
+                        if radio.get_ruid() not in g_RSSI_Measurements:
+                            g_RSSI_Measurements[radio.get_ruid()] = {}
                         else:
-                            radio.add_unassociated_station(unassoc_sta)
+                            if unassoc_sta.get_mac() not in g_RSSI_Measurements[radio.get_ruid()]:
+                                g_RSSI_Measurements[radio.get_ruid()][unassoc_sta.get_mac()] = []
+                            else:
+                                g_RSSI_Measurements[radio.get_ruid()][unassoc_sta.get_mac()].append(e['parameters']['SignalStrength'])
 
     # DEBUG
     # for i, agent in enumerate(agent_list):
@@ -1062,18 +1104,78 @@ def vbss_destruction_click(n_clicks: int, should_disassociate: bool, bssid: str)
     send_vbss_destruction_request(g_ControllerConnectionCtx, dummy_client_mac_addr, should_disassociate, bss)
     return f"Sent VBSS destruction request for '{bssid}'"
 
+
+def sta_has_moved(sta_mac: str) -> bool:
+    ret = False
+    if sta_mac in stations_have_moved:
+        ret = True
+        stations_have_moved.remove(sta_mac)
+    return ret
+
+#  station -> list of moves
+g_Transition_X_Positions = {}
 @app.callback(
     Output('rssi-plot', 'figure'),
     Input('rssi-plot-interval', 'n_intervals')
 )
-def update_rssi_plot(_):
+def update_rssi_plot(n_intervals: int):
     """Adds data points for the currently selected station in the 'rssi-plot' div.
 
     Args:
-        _ (n_intervals): Unused
+        int (n_intervals): Number of times this callback has fired.
     """
-    logging.debug("update_rssi_plot -- NOT IMPLEMENTED!")
-    return dict()
+    global RSSI_RELATIVE_TO_RADIO
+    station_of_interest = get_last_clicked_station()
+    if not station_of_interest:
+        # Log that there's no station selected once.
+        if (n_intervals == 0):
+            logging.debug("No station!")
+        return dict()
+    selected_sta_mac = station_of_interest.get_mac()
+    # Store as a static variable so we can clear the RSSI table when a new station is selected.
+    if update_rssi_plot.last_sta == None:
+        update_rssi_plot.last_sta = station_of_interest
+    if update_rssi_plot.last_sta.get_mac() != selected_sta_mac:
+        logging.debug(f"New station clicked ({selected_sta_mac}), resetting plot data")
+        update_rssi_plot.last_sta = station_of_interest
+    
+
+    fig = go.Figure()
+
+    unassoc_color_lut = {}
+    for key in g_RSSI_Measurements.items():
+        radio_mac = key[0]
+        agent = g_Topology.get_agent_by_ruid(radio_mac)
+        unassoc_color_lut[radio_mac] = get_color_for_agent(g_ColorSync, agent)
+
+    # We need to render curves starting at the most recent move, instead of t=0
+    initial_x_offset = 0
+    for ruid, sta_list in g_RSSI_Measurements.items():
+        for sta_mac, measurement_list in sta_list.items():
+            if sta_mac == selected_sta_mac:
+                y_axis_vals = measurement_list
+                a = max(len(measurement_list), 500)
+                x_axis_vals = list(range(initial_x_offset, a))
+                trace_name = f"STA {station_of_interest.params['Hostname']} ({selected_sta_mac}) relative to radio {ruid}"
+                trace = go.Scatter(x=x_axis_vals, y=y_axis_vals, marker=dict(color=unassoc_color_lut[ruid]), name=trace_name, mode="lines", hoverinfo="all")
+                fig.add_trace(trace)
+                if sta_has_moved(sta_mac):
+                    logging.debug(f"station has moved, drawing vertical marker.")
+                    if sta_mac not in g_Transition_X_Positions:
+                        g_Transition_X_Positions[sta_mac] = []
+                    pos = len(measurement_list) - 1
+                    g_Transition_X_Positions[sta_mac].append(pos)
+                    trace_name = f"VBSS move for STA {sta_mac}" 
+
+    # Must render each VBSS move every render cycle to maintain VBSS move history in the plot.
+    for station_mac, move_list in g_Transition_X_Positions.items():
+        if station_mac == selected_sta_mac:
+            for move_position in move_list:
+                logging.debug(f"move for STA {sta_mac} happened at {move_position}")
+                fig.add_vline(x=move_position, line_width=3, line_dash='dash', line_color='black')
+    return fig
+# Init callback function attribute (static)
+update_rssi_plot.last_sta = None
 
 if __name__ == '__main__':
     # Silence imported module logs
