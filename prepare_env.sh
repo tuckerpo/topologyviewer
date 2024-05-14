50d0a7114cebeba23a747360367d6d51ce751946
Maarten De Decker <maarten.dedecker@mind.be>
2024-05-14 12:28:06 +0200

    WIP: change IP/port
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 scriptdir="$(cd "${0%/*}"; pwd)"
 controller_addr=$(awk -F "=" '/controller-addr/ {print $2}' "$scriptdir/config.ini" | tr -d '\r')
+controller_port=$(awk -F "=" '/controller-port/ {print $2}' "$scriptdir/config.ini" | tr -d '\r')
 
 print_color() {
     echo -e "\e[0;33m$1\e[0m"
@@ -12,7 +13,7 @@ source "$scriptdir/bin/activate"
 python -m pip install -r "$scriptdir/requirements.txt"
 
 print_color "Getting session token from controller on ${controller_addr}:"
-session_id=$(curl --silent -X POST "http://${controller_addr}/session" --data '{"username":"admin","password":"admin"}' | jq -r .sessionID)
+session_id=$(curl --silent -X POST "http://${controller_addr}:${controller_port}/session" --data '{"username":"admin","password":"admin"}' | jq -r .sessionID)
 
 print_color "HTTP header if you want to use the session token:"
 print_color "Authorization: bearer ${session_id}"
