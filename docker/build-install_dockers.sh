084f1091e6d699b4171eeae9ae0a96a2b00f5384
Maarten De Decker <maarten.dedecker@mind.be>
2024-05-18 15:45:22 +0200

    adapt dockerfile
    Signed-off-by: Maarten De Decker <maarten.dedecker@mind.be>

--- docker/build-install_dockers.sh
+++ docker/build-install_dockers.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+scriptdir="$(cd "${0%/*}" || exit 1; pwd)"
 
 # Build OBUSPA USP test controller in docker
 # docker build -t obuspa_img OBUSPA/.
 
 # Checkout topologyviewer repository
-docker build -t topologyviewer_img .
+docker build -t topologyviewer_img ${scriptdir}
--- run_Topologyviewer.sh
+++ run_Topologyviewer.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Run the topologyviewer
-docker run -it --rm -p 8050:8050 --hostname "topology_server" --name "topology_runner" topologyviewer_img
+docker run -it --rm --net="host" -p8050:8050 --hostname "topology_server" --name "topology_runner" topologyviewer_img
